d20ef4d99f99f1df0e605c83eb704c6b64daf228
OneTribeHMU <140870774+OneTribeHMU@users.noreply.github.com>
2024-09-01 08:01:30 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Header from './Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import LanguageList from './components/LanguageList';
+import TeacherList from './components/TeacherList';
+import Schedule from './components/Schedule';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <h1>Welcome to AfricaSpeak OneTribe Polygot</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/languages" component={LanguageList} />
+          <Route path="/teachers" component={TeacherList} />
+          <Route path="/schedule" component={Schedule} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
